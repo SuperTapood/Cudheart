ccf939dca92f6dd6b3dc535e6ed48dfed7168a87
SuperTapood <59110744+SuperTapood@users.noreply.github.com>
2021-10-04 23:09:30 +0300

    fixed

--- Cudheart/Cudheart/src/Cudheart/Cudheart.h
+++ Cudheart/Cudheart/src/Cudheart/Cudheart.h
@@ -9,9 +9,9 @@ using namespace std;
 #include "CLib/CLib.h"
 
 namespace Cudheart {
-	int MAJOR_VERSION = 1;
+	int MAJOR_VERSION = 0;
 	int MINOR_VERSION = 0;
-	int PATCH_VERSION = 0;
+	int PATCH_VERSION = 1;
 	string version = to_string(MAJOR_VERSION) + "."
 		+ to_string(MINOR_VERSION) + "."
 		+ to_string(PATCH_VERSION);
