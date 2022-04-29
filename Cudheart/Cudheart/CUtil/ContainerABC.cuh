94d6306b964b2041a6e6ff148b9523a318a14475
SuperTapood <59110744+SuperTapood@users.noreply.github.com>
2022-04-29 16:21:49 +0300

    extended cudaException

--- Cudheart/Cudheart/CUtil/ContainerABC.cuh
+++ Cudheart/Cudheart/CUtil/ContainerABC.cuh
@@ -27,10 +27,10 @@ private:
 		cudaFree((void*)devC);
 	}
 
-	void checkStatus(cudaError_t status) {
+	void checkStatus(cudaError_t status, string func) {
 		if (status != cudaSuccess) {
 			release();
-			Cudheart::Exceptions::CudaException(cudaGetErrorName(status), cudaGetErrorString(status)).raise();
+			Cudheart::Exceptions::CudaException(status, func).raise();
 		}
 	}
 
@@ -41,31 +41,30 @@ public:
 		m_ptrA = a;
 		m_ptrB = b;
 		m_ptrC = c;
-		checkStatus(cudaErrorInvalidValue);
 		// allocate the needed memory on the gpu
 		{
-			checkStatus(cudaMalloc((void**)&devA, sizeof(T) * size));
-			checkStatus(cudaMalloc((void**)&devB, sizeof(T) * size));
-			checkStatus(cudaMalloc((void**)&devC, sizeof(T) * size));
+			checkStatus(cudaMalloc((void**)&devA, sizeof(T) * size), "cudaMalloc");
+			checkStatus(cudaMalloc((void**)&devB, sizeof(T) * size), "cudaMalloc");
+			checkStatus(cudaMalloc((void**)&devC, sizeof(T) * size), "cudaMalloc");
 		}
 		// copy data from cpu (host) memory to gpu (device) memory
 
 		{
-			checkStatus(cudaMemcpy(devA, m_ptrA, size * sizeof(T), cudaMemcpyHostToDevice));
-			checkStatus(cudaMemcpy(devB, m_ptrB, size * sizeof(T), cudaMemcpyHostToDevice));
-			checkStatus(cudaMemcpy(devC, m_ptrB, size * sizeof(T), cudaMemcpyHostToDevice));
+			checkStatus(cudaMemcpy(devA, m_ptrA, size * sizeof(T), cudaMemcpyHostToDevice), "cudaMemcpy of type host to device");
+			checkStatus(cudaMemcpy(devB, m_ptrB, size * sizeof(T), cudaMemcpyHostToDevice), "cudaMemcpy of type host to device");
+			checkStatus(cudaMemcpy(devC, m_ptrB, size * sizeof(T), cudaMemcpyHostToDevice), "cudaMemcpy of type host to device");
 		}
 	}
 
 	virtual void coolDown() {
 		// copy memory from the gpu back to the cpu
 		{
-			checkStatus(cudaMemcpy(m_ptrA, devA, m_size * sizeof(T), cudaMemcpyDeviceToHost));
-			checkStatus(cudaMemcpy(m_ptrB, devB, m_size * sizeof(T), cudaMemcpyDeviceToHost));
-			checkStatus(cudaMemcpy(m_ptrC, devC, m_size * sizeof(T), cudaMemcpyDeviceToHost));
+			checkStatus(cudaMemcpy(m_ptrA, devA, m_size * sizeof(T), cudaMemcpyDeviceToHost), "cudaMemcpy of type device to host");
+			checkStatus(cudaMemcpy(m_ptrB, devB, m_size * sizeof(T), cudaMemcpyDeviceToHost), "cudaMemcpy of type device to host");
+			checkStatus(cudaMemcpy(m_ptrC, devC, m_size * sizeof(T), cudaMemcpyDeviceToHost), "cudaMemcpy of type device to host");
 		}
 		// synchronize the device
-		checkStatus(cudaDeviceSynchronize());		
+		checkStatus(cudaDeviceSynchronize(), "cudaDeviceSynchronize");
 		release();
 	}
 };
--- Cudheart/Cudheart/Exceptions/CudaException.cuh
+++ Cudheart/Cudheart/Exceptions/CudaException.cuh
@@ -1,14 +1,30 @@
 #pragma once
 
 #include "BaseException.cuh"
+#include "driver_types.h"
+#include <iostream>
 
+using namespace std;
 
 namespace Cudheart::Exceptions {
 	class CudaException : public BaseException {
 	public:
-		CudaException(const char* name, const char* full) {
+		CudaException(cudaError_t error, string func) {
+			// add more exceptions as i encounter them
 			ostringstream os;
-			os << "CudaException: " << name << " " << full << endl;
+			os << "CudaException in '" << func << "':";
+			switch (error) {
+				case (cudaErrorInvalidValue):
+					os << "function got passed a value outside of an acceptable range";
+					break;
+				case (cudaErrorMemoryAllocation):
+					os << "cuda could not allocate enough memory in the GPU";
+					break;
+				case (cudaErrorInitializationError):
+					os << "cuda could not initialize the cuda driver and runtime";
+				default:
+					os << "unknown error " << cudaGetErrorString(error);
+			}
 			m_msg = os.str();
 		}
 	};
